f325044ddc4afa2a8db6cd8e82393cda9b5e71a4
VulcanMindForge <jastuart@live.com>
2023-09-22 16:51:54 -0700

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args){
+		String name = "Jacob Stuart";
+		int age = 36;
+		String state = "Arizona";
+
+		/*
+			System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+		*/
+	}
+}
